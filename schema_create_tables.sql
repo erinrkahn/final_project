8b9ed8f2cb03f014b6c9e7126b9df720b077dd15
amirshirazi1 <amirs914@gmail.com>
2021-06-27 14:32:52 -0700

    Processed data and schemas. Will clean later.

--- schema_create_tables.sql
+++ schema_create_tables.sql
@@ -1,32 +1,74 @@
 -- Creating tables for California Fires Final Project
-CREATE TABLE fire_incidents(
-	county VARCHAR() NOT NULL,
-	PRIMARY KEY (county),
-	UNIQUE()
+CREATE TABLE fire(
+	INDEX INT NOT NULL,
+	"Name" VARCHAR NOT NULL,
+	"Counties" VARCHAR NOT NULL,
+	"County IDs" INT NOT NULL,
+	"Latitude" FLOAT8 NOT NULL,
+	"Longitude" FLOAT8 NOT NULL,
+	"Started" VARCHAR NOT NULL,
+	"Year" INT NOT NULL,
+	"Acres Burned" FLOAT8,
+	"Cal Fire Incident" BOOL NOT NULL,
+	"Major Incident" BOOL NOT NULL
 );
 
-CREATE TABLE ca_env_factors(
-	
-    PRIMARY KEY (county)
+SELECT * FROM fire;
+
+ALTER TABLE fire 
+DROP COLUMN index;
+
+
+CREATE TABLE electricity(
+	index INT NOT NULL,
+	"County" VARCHAR NOT NULL,
+	"Year" INT NOT NULL,
+	"MWh" FLOAT8 NOT NULL
 );
 
-CREATE TABLE ca_ocean_proximity (
-	
-	FOREIGN KEY () REFERENCES  (),
-	PRIMARY KEY (county)
+SELECT * FROM electricity;
+
+ALTER TABLE electricity 
+DROP COLUMN index;
+
+CREATE TABLE housing(
+	index INT NOT NULL,
+	"County" VARCHAR NOT NULL,
+	"Latitude" FLOAT8 NOT NULL,
+	"Longitude" FLOAT8 NOT NULL,
+	"Ocean Proximity" VARCHAR NOT NULL,
+	"Population" INT NOT NULL,
+	"Households" INT NOT NULL,
+	"Average Income" INT NOT NULL,
+	"Average House Value" INT NOT NULL
 );
 
-CREATE TABLE ca_demographics (
+SELECT * FROM housing;
 
-	PRIMARY KEY (county)
+ALTER TABLE housing 
+DROP COLUMN index;
+
+
+CREATE TABLE enviornment(
+	index INT NOT NULL,
+	"County" VARCHAR NOT NULL,
+	"Year" INT NOT NULL,
+	"ETo (in)" FLOAT8 NOT NULL,
+	"Precip (in)" FLOAT8 NOT NULL,
+	"Sol Rad (Ly/day)" FLOAT8 NOT NULL,
+	"Avg Vap Pres (mBars)" FLOAT8 NOT NULL,
+	"Max Air Temp (F)" FLOAT8 NOT NULL,
+	"Min Air Temp (F)" FLOAT8 NOT NULL,
+	"Avg Air Temp (F)" FLOAT8 NOT NULL,
+	"Max Rel Hum (%)" FLOAT8 NOT NULL,
+	"Min Rel Hum (%)" FLOAT8 NOT NULL,
+	"Avg Rel Hum (%)" FLOAT8 NOT NULL,
+	"Dew Point (F)" FLOAT8 NOT NULL,
+	"Avg Wind Speed (mph)" FLOAT8 NOT NULL,
+	"Avg Soil Temp (F)" FLOAT8 NOT NULL
 );
 
-CREATE TABLE electricity_consumption (
-	
-	PRIMARY KEY (county)
-);
+SELECT * FROM enviornment;
 
-CREATE TABLE major_fire_risk(
-	
-	PRIMARY KEY (county)
-);
+ALTER TABLE enviornment 
+DROP COLUMN index;
